080ae754bd3bd663de825c3bf19f20a9c04af6f5
Laxman Desai <lolopaw24@gmail.com>
2022-01-13 00:23:31 +0530

    👌 IMPROVE: Setup base next rep

---
+++ packages/client/src/pages/_document.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable no-param-reassign */
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+
+    try {
+      const originalRenderPage = ctx.renderPage;
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
+
+export default MyDocument;
