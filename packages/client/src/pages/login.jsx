af682993c4da5e901d926122bb0735cd10aaea0e
Rishygunjal <rishygunjal@gmail.com>
2022-01-13 01:17:10 +0530

    👌IMPROVE: Added Loading feature implemented styled components

---
+++ packages/client/src/pages/login.jsx
@@ -0,0 +1,89 @@
+import { Form, Input, Button, Checkbox } from "antd";
+import { useRouter } from "next/router";
+
+const Demo = () => {
+  const router = useRouter();
+  const onFinish = (values) => {
+    console.log("Success:", values);
+    let r = (Math.random() + 1).toString(36).substring(2);
+    console.log("random", r);
+
+    router.push("/discuss/" + r);
+    // <Link href="/login">
+    //     <a>Hii</a>
+    // </Link>
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: "Please input your username!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+// ReactDOM.render(<Demo />, mountNode);
+export default Demo;
---
+++ packages/client/src/pages/signup.jsx
@@ -0,0 +1,134 @@
+import { Form, Input, Button, Checkbox } from "antd";
+import { useRouter } from "next/router";
+import axios from "axios";
+import styled from "styled-components";
+import { useState } from "react";
+
+const Demo = () => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const onFinish = async (values) => {
+    // console.log(
+    //   "Success:",
+    //   values,
+    //   values["password"],
+    //   values["Confirm password"]
+    // );
+    try {
+      // make axios post request
+      setLoading(true);
+      const response = await axios({
+        method: "post",
+        url: "https://httpstat.us/200?sleep=5000",
+        data: values,
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+
+    if (values["password"] == values["Confirm password"]) {
+      router.push("/login");
+    } else {
+      alert("Password doesn't match");
+    }
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <StyledForm
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: "Please input your username!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item
+        label="Confirm Password"
+        name="Confirm password"
+        rules={[
+          {
+            required: true,
+            message: "Confirm your password!",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit" loading={loading}>
+          Submit
+        </Button>
+      </Form.Item>
+    </StyledForm>
+  );
+};
+
+export default Demo;
+
+const StyledForm = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  margin-bottom: 1rem;
+  padding: 1rem;
+  background: ${({ theme }) => theme.secondary};
+  border-radius: ${({ theme }) => theme.borderRadius};
+  box-shadow: 0 0 0.5rem rgb(0 0 0 / 50%);
+`;
