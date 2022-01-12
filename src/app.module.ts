9ef6c972acf6a078fabfc703fdeba9ef28080d88
harsh-kr-jha <harshatmagicalstud@gmail.com>
2022-01-12 14:29:58 +0530

    Initial backend commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactsModule } from './contacts/contacts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from 'auth/auth/auth.service';
+import { AuthController } from 'auth/auth/auth.controller';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'auth/user/user.service';
+import { User } from 'auth/user.entity';
+import { Contact } from 'contacts/contact.entity';
 
 @Module({
-  imports: [ContactsModule,       
-  TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true,
- }),],
+  imports: [ContactsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'users',
+      entities: [User, Contact],
+      synchronize: true,
+    }), AuthModule,
+    TypeOrmModule.forFeature([User]),],
   controllers: [AppController],
   providers: [AppService],
+
 })
-export class AppModule {}
+export class AppModule { }
